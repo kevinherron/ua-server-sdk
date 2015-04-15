9f186c87c6eca7d3f15d73542888763ec73ec6b8
Kevin Herron <kevinherron@gmail.com>
2015-04-15 07:38:06 -0700

    Update example

--- sdk-examples/src/main/java/com/inductiveautomation/opcua/sdk/examples/OpcUaClientExample.java
+++ sdk-examples/src/main/java/com/inductiveautomation/opcua/sdk/examples/OpcUaClientExample.java
@@ -17,8 +17,13 @@
 package com.inductiveautomation.opcua.sdk.examples;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
+import com.google.common.collect.Lists;
 import com.inductiveautomation.opcua.sdk.client.OpcUaClient;
 import com.inductiveautomation.opcua.sdk.client.OpcUaClientConfig;
 import com.inductiveautomation.opcua.sdk.client.subscriptions.OpcUaMonitoredItem;
@@ -27,7 +32,9 @@ import com.inductiveautomation.opcua.sdk.client.subscriptions.SubscriptionManage
 import com.inductiveautomation.opcua.stack.client.UaTcpStackClientConfig;
 import com.inductiveautomation.opcua.stack.client.UaTcpStackClient;
 import com.inductiveautomation.opcua.stack.core.Identifiers;
+import com.inductiveautomation.opcua.stack.core.types.builtin.DataValue;
 import com.inductiveautomation.opcua.stack.core.types.builtin.LocalizedText;
+import com.inductiveautomation.opcua.stack.core.types.builtin.NodeId;
 import com.inductiveautomation.opcua.stack.core.types.builtin.QualifiedName;
 import com.inductiveautomation.opcua.stack.core.types.builtin.unsigned.UInteger;
 import com.inductiveautomation.opcua.stack.core.types.enumerated.MessageSecurityMode;
@@ -71,38 +78,52 @@ public class OpcUaClientExample {
 
         client.connect().get();
 
-        SubscriptionManager subscriptionManager = new SubscriptionManager(client);
+//        SubscriptionManager subscriptionManager = new SubscriptionManager(client);
+//
+//
+//        OpcUaSubscription subscription = new OpcUaSubscription(
+//                client,
+//                1000.0,
+//                uint(15),
+//                uint(5),
+//                uint(0),
+//                true,
+//                ubyte(0));
+//
+//        UInteger clientHandle = subscriptionManager.nextClientHandle();
+//
+//        ReadValueId readValueId = new ReadValueId(
+//                Identifiers.Server_ServerStatus_CurrentTime,
+//                uint(13), null, QualifiedName.NULL_VALUE);
+//
+//        OpcUaMonitoredItem item = new OpcUaMonitoredItem(clientHandle, readValueId);
+//
+//        subscriptionManager.addSubscription(subscription).thenCompose(s -> {
+//            return subscription.createItems(TimestampsToReturn.Both, createItemsContext -> {
+//                MonitoringParameters parameters = new MonitoringParameters(
+//                        uint(1),
+//                        250.0,
+//                        null,
+//                        uint(5),
+//                        true);
+//
+//                createItemsContext.addItem(item, parameters, MonitoringMode.Reporting);
+//            });
+//        }).thenAccept(results -> results.forEach(System.out::println));
 
+        List<NodeId> nodeIds = Collections.nCopies(1000, Identifiers.Server_ServerStatus_CurrentTime);
 
-        OpcUaSubscription subscription = new OpcUaSubscription(
-                client,
-                1000.0,
-                uint(15),
-                uint(5),
-                uint(0),
-                true,
-                ubyte(0));
+        long timeA = System.nanoTime();
+        CompletableFuture<?>[] futures = new CompletableFuture<?>[1000];
 
-        UInteger clientHandle = subscriptionManager.nextClientHandle();
+        for (int i = 0; i < 1000; i++) {
+            futures[i] = client.readValues(0.0d, TimestampsToReturn.Both, nodeIds);
+        }
 
-        ReadValueId readValueId = new ReadValueId(
-                Identifiers.Server_ServerStatus_CurrentTime,
-                uint(13), null, QualifiedName.NULL_VALUE);
+        CompletableFuture.allOf(futures).get();
+        long timeB = System.nanoTime();
 
-        OpcUaMonitoredItem item = new OpcUaMonitoredItem(clientHandle, readValueId);
-
-        subscriptionManager.addSubscription(subscription).thenCompose(s -> {
-            return subscription.createItems(TimestampsToReturn.Both, createItemsContext -> {
-                MonitoringParameters parameters = new MonitoringParameters(
-                        uint(1),
-                        250.0,
-                        null,
-                        uint(5),
-                        true);
-
-                createItemsContext.addItem(item, parameters, MonitoringMode.Reporting);
-            });
-        }).thenAccept(results -> results.forEach(System.out::println));
+        System.out.println(String.format("Took %sms", TimeUnit.MILLISECONDS.convert(timeB-timeA, TimeUnit.NANOSECONDS)));
 
         Thread.sleep(999999999);
     }
