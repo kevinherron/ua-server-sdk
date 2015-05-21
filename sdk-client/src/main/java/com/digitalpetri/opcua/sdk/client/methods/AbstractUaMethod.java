27386dfeaefdc2a0788b30970c8784b0803cc0e6
Kevin Herron <kevinherron@gmail.com>
2015-05-20 20:32:15 -0700

    GetMonitoredItems and AbstractUaMethod

---
+++ sdk-client/src/main/java/com/digitalpetri/opcua/sdk/client/methods/AbstractUaMethod.java
@@ -0,0 +1,74 @@
+/*
+ * digitalpetri OPC-UA SDK
+ *
+ * Copyright (C) 2015 Kevin Herron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.digitalpetri.opcua.sdk.client.methods;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.digitalpetri.opcua.sdk.client.api.UaClient;
+import com.digitalpetri.opcua.stack.core.UaException;
+import com.digitalpetri.opcua.stack.core.types.builtin.DiagnosticInfo;
+import com.digitalpetri.opcua.stack.core.types.builtin.NodeId;
+import com.digitalpetri.opcua.stack.core.types.builtin.StatusCode;
+import com.digitalpetri.opcua.stack.core.types.builtin.Variant;
+import com.digitalpetri.opcua.stack.core.types.structured.CallMethodRequest;
+
+public abstract class AbstractUaMethod {
+
+    private final UaClient client;
+    private final NodeId objectId;
+    private final NodeId methodId;
+
+    public AbstractUaMethod(UaClient client, NodeId objectId, NodeId methodId) {
+        this.client = client;
+        this.objectId = objectId;
+        this.methodId = methodId;
+    }
+
+    public CompletableFuture<Variant[]> invoke(Variant[] inputArguments) {
+        CallMethodRequest request = new CallMethodRequest(
+                objectId, methodId, inputArguments);
+
+        return client.call(request).thenCompose(result -> {
+            StatusCode statusCode = result.getStatusCode();
+
+            if (statusCode.isGood()) {
+                Variant[] outputArguments = result.getOutputArguments();
+
+                return CompletableFuture.completedFuture(outputArguments);
+            } else {
+                return createExceptionFuture(
+                        statusCode,
+                        result.getInputArgumentResults(),
+                        result.getInputArgumentDiagnosticInfos());
+            }
+        });
+    }
+
+    protected <T> CompletableFuture<T> createExceptionFuture(StatusCode statusCode,
+                                                             StatusCode[] inputArgumentResults,
+                                                             DiagnosticInfo[] inputArgumentDiagnosticInfos) {
+
+        // TODO UaMethodException
+        CompletableFuture<T> f = new CompletableFuture<>();
+        f.completeExceptionally(new UaException(statusCode));
+        return f;
+    }
+
+}
---
+++ sdk-client/src/main/java/com/digitalpetri/opcua/sdk/client/methods/GetMonitoredItems.java
@@ -0,0 +1,62 @@
+/*
+ * digitalpetri OPC-UA SDK
+ *
+ * Copyright (C) 2015 Kevin Herron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.digitalpetri.opcua.sdk.client.methods;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.digitalpetri.opcua.sdk.client.api.UaClient;
+import com.digitalpetri.opcua.stack.core.types.builtin.NodeId;
+import com.digitalpetri.opcua.stack.core.types.builtin.Variant;
+import com.digitalpetri.opcua.stack.core.types.builtin.unsigned.UInteger;
+import org.jooq.lambda.tuple.Tuple2;
+
+public class GetMonitoredItems extends AbstractUaMethod {
+
+    public GetMonitoredItems(UaClient client, NodeId objectId, NodeId methodId) {
+        super(client, objectId, methodId);
+    }
+
+    /**
+     * GetMonitoredItems is used to get information about monitored items of a subscription.
+     *
+     * @param subscriptionId identifier of the subscription.
+     * @return a {@link Tuple2} containing the output arguments.
+     * <p>
+     * serverHandles (UInt32[]) - array of serverHandles for all MonitoredItems of the subscription identified by
+     * subscriptionId.
+     * <p>
+     * clientHandles (UInt32[]) - array of clientHandles for all MonitoredItems of the subscription identified by
+     * subscriptionId.
+     */
+    public CompletableFuture<Tuple2<UInteger[], UInteger[]>> invoke(UInteger subscriptionId) {
+        Variant[] inputArguments = new Variant[]{
+                new Variant(subscriptionId)
+        };
+
+        return invoke(inputArguments).thenApply(outputArguments -> {
+            UInteger[] v0 = (UInteger[]) outputArguments[0].getValue();
+            UInteger[] v1 = (UInteger[]) outputArguments[1].getValue();
+
+            return new Tuple2<>(v0, v1);
+        });
+    }
+
+
+}
