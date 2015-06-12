0f2f04b71c752f2ac8a4864f41cd5da694bf705f
Kevin Herron <kevinherron@gmail.com>
2015-06-12 06:20:15 -0700

    Subscription transfer IT

--- sdk-tests/src/test/java/com/digitalpetri/opcua/sdk/client/OpcUaClientIT.java
+++ sdk-tests/src/test/java/com/digitalpetri/opcua/sdk/client/OpcUaClientIT.java
@@ -5,19 +5,25 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import com.digitalpetri.opcua.sdk.client.api.UaSession;
 import com.digitalpetri.opcua.sdk.client.api.config.OpcUaClientConfig;
 import com.digitalpetri.opcua.sdk.client.api.nodes.attached.UaVariableNode;
 import com.digitalpetri.opcua.sdk.client.api.subscriptions.UaMonitoredItem;
 import com.digitalpetri.opcua.sdk.client.api.subscriptions.UaSubscription;
+import com.digitalpetri.opcua.sdk.client.api.subscriptions.UaSubscriptionManager.SubscriptionListener;
 import com.digitalpetri.opcua.sdk.core.AttributeId;
 import com.digitalpetri.opcua.sdk.server.OpcUaServer;
 import com.digitalpetri.opcua.sdk.server.api.config.OpcUaServerConfig;
 import com.digitalpetri.opcua.server.ctt.CttNamespace;
 import com.digitalpetri.opcua.stack.client.UaTcpStackClient;
 import com.digitalpetri.opcua.stack.core.Identifiers;
+import com.digitalpetri.opcua.stack.core.UaException;
 import com.digitalpetri.opcua.stack.core.security.SecurityPolicy;
 import com.digitalpetri.opcua.stack.core.types.builtin.DataValue;
+import com.digitalpetri.opcua.stack.core.types.builtin.DateTime;
 import com.digitalpetri.opcua.stack.core.types.builtin.LocalizedText;
 import com.digitalpetri.opcua.stack.core.types.builtin.NodeId;
 import com.digitalpetri.opcua.stack.core.types.builtin.QualifiedName;
@@ -32,14 +38,13 @@ import com.digitalpetri.opcua.stack.core.types.structured.ReadValueId;
 import com.digitalpetri.opcua.stack.server.tcp.SocketServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static com.digitalpetri.opcua.stack.core.types.builtin.unsigned.Unsigned.uint;
 import static com.google.common.collect.Lists.newArrayList;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
@@ -164,6 +169,82 @@ public class OpcUaClientIT {
         item.setValueConsumer(f::complete);
 
         assertNotNull(f.get(5, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void testTransferSubscriptions() throws Exception {
+        logger.info("testTransferSubscriptions()");
+
+        // create a subscription and a monitored item
+        UaSubscription subscription = client.getSubscriptionManager().createSubscription(1000.0).get();
+
+        NodeId nodeId = new NodeId(2, "/Static/AllProfiles/Scalar/Int32");
+
+        ReadValueId readValueId = new ReadValueId(
+                nodeId, AttributeId.VALUE.uid(),
+                null, QualifiedName.NULL_VALUE);
+
+        MonitoringParameters parameters = new MonitoringParameters(
+                uint(1),    // client handle
+                100.0,      // sampling interval
+                null,       // no (default) filter
+                uint(10),   // queue size
+                true);      // discard oldest
+
+        MonitoredItemCreateRequest request = new MonitoredItemCreateRequest(
+                readValueId, MonitoringMode.Reporting, parameters);
+
+        List<UaMonitoredItem> items = subscription
+                .createMonitoredItems(TimestampsToReturn.Both, newArrayList(request)).get();
+
+        // do something with the value updates
+        UaMonitoredItem item = items.get(0);
+
+        AtomicInteger updateCount = new AtomicInteger(0);
+
+        item.setValueConsumer(v -> {
+            int count = updateCount.incrementAndGet();
+            logger.info("updateCount={}", count);
+        });
+
+        AtomicBoolean subscriptionTransferred = new AtomicBoolean(true);
+
+        client.getSubscriptionManager().addSubscriptionListener(new SubscriptionListener() {
+            @Override
+            public void onKeepAlive(UaSubscription subscription, DateTime publishTime) {
+
+            }
+
+            @Override
+            public void onStatusChanged(UaSubscription subscription, StatusCode status) {
+
+            }
+
+            @Override
+            public void onPublishFailure(UaException exception) {
+
+            }
+
+            @Override
+            public void onNotificationDataLost(UaSubscription subscription) {
+
+            }
+
+            @Override
+            public void onSubscriptionTransferFailed(UaSubscription subscription, StatusCode statusCode) {
+                subscriptionTransferred.set(false);
+            }
+        });
+
+        UaSession uaSession = client.getSession().get();
+        server.getSessionManager().killSession(uaSession.getSessionId(), false);
+
+        Thread.sleep(2000);
+
+        // one update for the initial subscribe, another after transfer
+        assertEquals(updateCount.get(), 2);
+
+        assertTrue(subscriptionTransferred.get());
     }
 
 }
